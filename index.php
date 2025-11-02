2eadb17c9d2e71d75afdc8998b5df459595af707
David Novotný <davidnovotny47@gmail.com>
2025-11-02 10:36:53 +0100

    hide unused taxs

--- index.php
+++ index.php
@@ -18,16 +18,16 @@ if (!defined('ABSPATH')) {
 }
 
 include_once('includes/product-post-type.php');
-include_once('includes/product-category-tax.php');
-include_once('includes/product-brand-tax.php');
+// include_once('includes/product-category-tax.php');
+// include_once('includes/product-brand-tax.php');
 include_once('includes/enqueue-assets.php');
 
 
 function _themename__pluginname_activate()
 {
     _themename__pluginname_setup_post_type();
-    _themename__pluginname_register_product_category_tax();
-    _themename__pluginname_register_product_brand_tax();
+    // _themename__pluginname_register_product_category_tax();
+    // _themename__pluginname_register_product_brand_tax();
     flush_rewrite_rules();
 }
 
@@ -36,8 +36,8 @@ register_activation_hook(__FILE__, '_themename__pluginname_activate');
 function _themename__pluginname_deactivate()
 {
     unregister_post_type('_themename_product');
-    unregister_taxonomy('_themename_product_category');
-    unregister_taxonomy('_themename_product_brand');
+    // unregister_taxonomy('_themename_product_category');
+    // unregister_taxonomy('_themename_product_brand');
     flush_rewrite_rules();
 }
 
